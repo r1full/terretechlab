e1a916b114e08166944c80a15cb62ecfad2b68ff
r1full_ <155621677+r1full@users.noreply.github.com>
2025-10-13 20:47:17 +0900

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  const loadingScreen = document.getElementById("loadingScreen");
+  setTimeout(() => {
+    loadingScreen.classList.add("fadeOut");
+    setTimeout(() => {
+      loadingScreen.remove();
+    }, 1000);
+  }, 2000);
+});
