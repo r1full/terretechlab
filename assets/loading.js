060c1f1c902ccfb1ae1d2d52bf6a74efa8562b7a
r1full_ <155621677+r1full@users.noreply.github.com>
2025-10-15 20:31:32 +0900

    Update loading.js

--- assets/loading.js
+++ assets/loading.js
@@ -1,9 +1,20 @@
 window.addEventListener("load", () => {
   const loadingScreen = document.getElementById("loadingScreen");
-  setTimeout(() => {
-    loadingScreen.classList.add("fadeOut");
+
+  const hasVisited = document.cookie.includes("visited=true");
+
+  if (!hasVisited) {
     setTimeout(() => {
-      loadingScreen.remove();
-    }, 1000);
-  }, 500);
+      loadingScreen.classList.add("fadeOut");
+      setTimeout(() => {
+        loadingScreen.remove();
+      }, 1000);
+    }, 500);
+    const expireDays = 30;
+    const d = new Date();
+    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    document.cookie = `visited=true; expires=${d.toUTCString()}; path=/`;
+  } else {
+    loadingScreen.remove();
+  }
 });
